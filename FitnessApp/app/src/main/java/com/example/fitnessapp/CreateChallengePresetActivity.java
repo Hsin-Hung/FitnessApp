26f8bf887189a19e11d608162754d1afc2a2d7aa
Hsin-Hung Wu <henrywu19970511@gmail.com>
2021-04-07 19:54:25 +0800

    add more screens

--- FitnessApp/app/src/main/java/com/example/fitnessapp/CreateChallengePresetActivity.java
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/CreateChallengePresetActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CreateChallengeActivity extends AppCompatActivity {
+public class CreateChallengePresetActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_challenge);
+        setContentView(R.layout.activity_create_challenge_preset);
     }
 }
