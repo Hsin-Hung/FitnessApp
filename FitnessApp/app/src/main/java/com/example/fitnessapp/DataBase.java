3bd243530a4e4519429c3bd5d986f5744c39dc1f
Hsin-Hung Wu <henrywu19970511@gmail.com>
2021-04-07 20:04:52 +0800

    add firebase & user class

---
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/DataBase.java
@@ -0,0 +1,19 @@
+package com.example.fitnessapp;
+
+// All the firebase calling action will stay in this class
+public class DataBase {
+
+    public DataBase(){
+
+    }
+
+    public UserAccount getUser(int accountID){
+
+
+
+
+        return null;
+
+    }
+
+}
---
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/UserAccount.java
@@ -0,0 +1,20 @@
+package com.example.fitnessapp;
+
+public class UserAccount {
+
+    String name;
+    String email;
+    // ......
+
+    public UserAccount(){
+
+
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    //TODO- add more methods 
+
+}
