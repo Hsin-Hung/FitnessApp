c4b5cbeffd1d300c70ecd46edd48e5f2f1d45ab4
Hsin-Hung Wu <henrywu19970511@gmail.com>
2021-04-10 05:38:13 +0800

    add skeleton code for create chall

--- FitnessApp/app/src/main/java/com/example/fitnessapp/CreateChallengePresetActivity.java
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/CreateChallengePresetActivity.java
@@ -3,12 +3,21 @@ package com.example.fitnessapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 
 public class CreateChallengePresetActivity extends AppCompatActivity {
+
+    EditText roomNameET, challDescrET, endDateET, betAmountET, passwordET;
+    Spinner challTypeSpinner;
+    RadioButton betRBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_challenge_preset);
     }
+
+
 }
